498b1d48848ef664ffdfd3f7bafddcb60e1ecef7
Aitor Ruiz <aitorruiz2m@gmail.com>
2020-11-22 17:38:04 +0100

    Añadida una nueva cancion. Eliminado el cache. Mejorado el reproductor

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,39 +1,63 @@
-let playing = true;
-var song;
+var song = null;
+var idPlayer = null;
+var iconList = null;
+var iconGeneric = null;
 
 function PlayPause(id) {
-    song = document.getElementById(id + "audioPlayer");
-
-    try {
-        if (document.getElementById(id + "Icon").className == "far fa-play-circle") {
-            document.getElementById(id + "Icon").className = "far fa-pause-circle";
-            document.getElementById("genericPlayer").className = "far fa-pause-circle";
-            if (document.getElementById(id + "audioPlayer").paused){
-                document.getElementById(id + "audioPlayer").play();
-                playing = false;
-            } else {
-                document.getElementById(id + "audioPlayer").load()
-                document.getElementById(id + "audioPlayer").play()
-                playing = false;
+    if(song == null || idPlayer == id){ // If song is null is a new song. If the id is the same just pause
+        song = document.getElementById(id + "audioPlayer"); // Store data
+        iconList = document.getElementById(id + "Icon");
+        iconGeneric = document.getElementById("genericPlayer");
+        idPlayer = id;
+        try {
+            if (iconList.className == "far fa-play-circle") {
+                iconList.className = "far fa-pause-circle";
+                iconGeneric.className = "far fa-pause-circle";
+                if (song.paused){
+                    song.play();
+                } else {
+                    song.load()
+                    song.play()
+                }
+            } else if (iconList.className == "far fa-pause-circle"){
+                iconList.className = "far fa-play-circle";
+                iconGeneric.className = "far fa-play-circle";
+                song.pause()
             }
-        } else if (document.getElementById(id + "Icon").className == "far fa-pause-circle"){
-            document.getElementById(id + "Icon").className = "far fa-play-circle";
-            document.getElementById("genericPlayer").className = "far fa-play-circle";
-            document.getElementById(id + "audioPlayer").pause()
-            playing = true;
+            
+        } catch (error) {
+            console.log(error);
         }
-        
-    } catch (error) {
-        console.log(error);
-    } 
+    } else {
+        song.pause()                                        // Pause the current playing song
+        song.currentTime = 0;                               // Resets the song
+        iconList.className = "far fa-play-circle";          // Edit the CSS to make sense
+        iconGeneric.className = "far fa-play-circle";       //
+        song = document.getElementById(id + "audioPlayer"); // Update the song player
+        idPlayer = id;                                      // Update the id of the player
+        PlayPause(id);                                      // Restart the func
+    }
+}
+function genericPlayPause() {
+    if (song.paused){
+        song.play();
+        iconList.className = "far fa-pause-circle";
+        iconGeneric.className = "far fa-pause-circle";
+    } else {
+        song.pause();
+        iconList.className = "far fa-play-circle";
+        iconGeneric.className = "far fa-play-circle";
+    }
 }
 function updateProgressValue() {
     if(song != null){
-        var percent = (song.duration * song.currentTime) / 100;
+        var percent = (song.currentTime * 100) / song.duration;
+        console.log("Duración: " + song.duration + "\n Current: " + song.currentTime + "\n(Current*100/duracion): " + percent + "%");
         $('.progress-bar').css('width', percent+'%').attr('aria-valuenow', percent);
+        if (song.currentTime == song.duration){
+            iconList.className = "far fa-play-circle";
+            iconGeneric.className = "far fa-play-circle";
+        }
     }
 };
-function changeProgressBar() {
-    song.currentTime = progressBar.value;
-};
-setInterval(updateProgressValue, 500);
+setInterval(updateProgressValue, 200); // Update the progress of the bar
