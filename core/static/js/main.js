8fab0eb2e65996c759c16100e284e517a0f12f22
Aitor Ruiz <aitorruiz2m@gmail.com>
2020-11-23 11:45:56 +0100

    Repeat and auto next

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -2,13 +2,20 @@ var song = null;
 var idPlayer = null;
 var iconList = null;
 var iconGeneric = null;
+var totalTable = null;
+var currentSongIndex = null;
 
 function PlayPause(id) {
     if(song == null || idPlayer == id){ // If song is null is a new song. If the id is the same just pause
+        idPlayer = id;
+        for (let i = 0; i < totalTable.length; i++) {
+            if(totalTable[i].id == idPlayer){
+                currentSongIndex = i;
+            }
+        }
         song = document.getElementById(id + "audioPlayer"); // Store data
         iconList = document.getElementById(id + "Icon");
         iconGeneric = document.getElementById("genericPlayer");
-        idPlayer = id;
         try {
             if (iconList.className == "far fa-play-circle") {
                 iconList.className = "far fa-pause-circle";
@@ -16,8 +23,9 @@ function PlayPause(id) {
                 if (song.paused){
                     song.play();
                 } else {
-                    song.load()
-                    song.play()
+                    song.load();
+                    song.play();
+                    song.volume = 0.2;
                 }
             } else if (iconList.className == "far fa-pause-circle"){
                 iconList.className = "far fa-play-circle";
@@ -49,15 +57,30 @@ function genericPlayPause() {
         iconGeneric.className = "far fa-play-circle";
     }
 }
+function nextSong(index) {
+    console.log("i: " + currentSongIndex + "\nindex: " + index + "\ni + index: " + (currentSongIndex+index));
+    let nextId = totalTable[currentSongIndex + index].id;
+    PlayPause(nextId);
+}
 function updateProgressValue() {
     if(song != null){
         var percent = (song.currentTime * 100) / song.duration;
-        console.log("Duración: " + song.duration + "\n Current: " + song.currentTime + "\n(Current*100/duracion): " + percent + "%");
+        // console.log("Duración: " + song.duration + "\n Current: " + song.currentTime + "\n(Current*100/duracion): " + percent + "%");
         $('.progress-bar').css('width', percent+'%').attr('aria-valuenow', percent);
         if (song.currentTime == song.duration){
             iconList.className = "far fa-play-circle";
             iconGeneric.className = "far fa-play-circle";
+            console.log("Current index: " + currentSongIndex + "\nLength: " + (totalTable.length - 1));
+            if(currentSongIndex == (totalTable.length - 1)){
+                let index = -Math.abs(currentSongIndex)
+                nextSong(index);
+            } else {
+                nextSong(1);
+            }
         }
     }
 };
 setInterval(updateProgressValue, 200); // Update the progress of the bar
+$(document).ready(function() {
+    totalTable = $('.table tr td a');
+});
