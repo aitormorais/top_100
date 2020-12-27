6d35fccccb8e3c025a313e2b4c87da447ce0195c
lucas deprit <33033880+lucasdeprit@users.noreply.github.com>
2020-12-27 13:46:55 +0100

    mejoras en admin.py
    listas ordenadas por nombre y algun parametro

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from core.models import Estilo, Artista, Cancion
 
-admin.site.register(Estilo)
-admin.site.register(Artista)
-admin.site.register(Cancion)
+
+class EstiloAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ['nombre']
+
+
+class ArtistaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ['nombre']
+
+
+class CancionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'estilo', 'artista')
+    search_fields = ['nombre']
+
+
+admin.site.register(Estilo, EstiloAdmin)
+admin.site.register(Artista, ArtistaAdmin)
+admin.site.register(Cancion, CancionAdmin)
